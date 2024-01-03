a1ec92d0109a834c161f5ff63e52164b27458bf3
Kholov Rustam <rustamkholow5@gmail.com>
2024-01-03 21:05:25 +0500

    hw2.5

--- script.js
+++ script.js
@@ -1,282 +1,171 @@
-// Д.з . 2.3 
-
-// //  Задание 1
-
-// let UserPassword = prompt('Введите пароль');
-// let password = 'пароль';
-
-// UserPassword = UserPassword.toLowerCase();
-
-// if (UserPassword === password.toLowerCase()) {
-//     console.log('Пароль введен верно');
-// } else {
-//     console.log('Пароль введен неправильно');
-// }
-
-// // // Задание 2 
-
-// let c = Number(prompt('Введите число'));
-
-// if (c >= 0 && c <= 10) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
-// }
-
-// // Задание 3 
-
-// let d = Number(prompt('Введите первое число'));
-// let e = Number(prompt('Введите второе число'));
-
-// if (d > 100 || e > 100) {
-//     console.log('Верно');
-// } else {
-//     console.log('Неверно');
-// }
-
-// // Задание 4 
-// // a
-// let a = '2';
-// let b = '3';
-// alert(Number (a) + Number(b)); 
-// // b
-// alert(+a + +b);
-
-// // Задание 5 
-
-// let month = prompt('Введите номер месяца');
-
-// if (month !== null && !isNaN(month)) {
-//     switch (month) {
-//         case '1':
-//             console.log('Январь');
-//             break;
-//         case '2':
-//             console.log('Февраль');
-//             break;
-//         case '3':
-//             console.log('Март');
-//             break;
-//         case '4':
-//             console.log('Апрель');
-//             break;
-//         case '5':
-//             console.log('Май');
-//             break;
-//         case '6':
-//             console.log('Июнь');
-//             break;
-//         case '7':
-//             console.log('Июль');
-//             break;
-//         case '8':
-//             console.log('Август');
-//             break;
-//         case '9':
-//             console.log('Сентябрь');
-//             break;
-//         case '10':
-//             console.log('Открябрь');
-//             break;
-//         case '11':
-//             console.log('Ноябрь');
-//             break;
-//         case '12':
-//             console.log('Декабрь');
-//             break;
-//         default: console.log('Введено некорректное число');
-//             break;
-//     }
-    
-// } else {
-//     console.log('Ввод отменен или введено не число');
-// }
-
-
-// // Задание 7 
-
-// let inputNumber = prompt('Ввидите любое число');
-
-// if (isNaN(inputNumber)) {
-//     console.log('Это не число');
-// } else {
-//     if (inputNumber % 2 === 0) {
-//         console.log('Это число четное');
-//     } else {
-//         console.log('Это число нечетное');
-//     }
-// }
-
-// // Задание 8 
-// let clientOS = Number(prompt('Введите 0, если вы используете iOS, или 1, если вы используете Android'));
-
-// if (clientOS === 0) {
-//     console.log('Установите версию приложения для iOS по ссылке');
-// } else if (clientOS === 1){
-//     console.log('Установите версию приложения для Android по ссылке');
-// } else {
-//     console.log('Некорректное значение. Введите 0 для iOS или 1 для Android.');
-// }
-
-// // Задание 9 
-
-// let clientOS = Number(prompt('Введите 0, если вы используете iOS, или 1, если вы используете Android'));
-// let yearDivice = Number(prompt('Укажите год выпуска Вашего дивайса'));
-
-// if (clientOS === 0) {
-//     if (yearDivice >= 2015) {
-//         console.log('Установите версию приложения для iOS по ссылке');
-//     } else {
-//         console.log('Установите облегченную версию приложения для iOS по ссылке');
-//     }
-// } else {
-//     if (yearDivice >= 2015) {
-//         console.log('Установите приложение для Android по ссылке');
-//     } else {
-//         console.log('Установите облегченную версию приложения для Android по ссылке');
-//     }
-// }
-
-
-// ............................................................................................................
-
-// Д.з  2.4 
-
+// //  Д.з 2.5 
 // //  Задание 1 
 
 // // a
-// let x = 0;
-// while (x < 2) {
-//     console.log('Привет');
-//     x++;
+
+// function findLess(a, b) {
+//   if (a > b) {
+//       return b;
+//   } else {
+//       return b;
+//   }
 // }
+
+// console.log(findLess(6, 6));
 
 // // b
-// let y = 0;
-// do {
-//     console.log('Привет');
-//     y++
-// } while (y < 2);
 
-// // c
-// for (let z = 0; z < 2; z++) {
-//     console.log('Привет');
-    
+// function findLess(a, b) {
+//   return a <= b ? a : b;
 // }
 
-// // Задание 2
+// console.log(findLess(8, 4));
+
+// // c 
+
+// function findLess(a, b) {
+//   return Math.min(a, b);
+// }
+
+// console.log(findLess(10, 22));
+
+// //  Задание 2
 
 // // a
-// let a = 1;
-// while (a <= 5) {
-//     console.log(a);
-//     a+= 1;
+
+// function findEvenNumber(a) {
+//   if (a % 2 === 0) {
+//     return ' Число четное'
+//   } else {
+//     return 'Число нечетное'
+//   }
 // }
 
-// // b 
-// let b = 1;
-// do {
-//     console.log(b);
-//     b = b + 1;
-// } while (b <= 5);
+// console.log(findEvenNumber(7));
 
-// // c 
-// for (c = 1; c <= 5; c++) {
-//     console.log(c);
+// // b
+
+// function findEvenNumber(a) {
+//   return a % 2 === 0 ? ' Число четное' : 'Число нечетное';
 // }
 
-// // Задание 3 
+// // // Задание 3 
 
-// // a 
-// let d = 7;
-// while (d <= 22) {
-//     console.log(d);
-//     d+= 1
+// // // 3.1
+
+// function squareOfTheNumber(b) {
+//   const square = b * b;
+//   console.log(square);
 // }
 
-// // b 
-// let e = 7;
-// do {
-//     console.log(e);
-//     e = e + 1
-// } while (e <= 22);
+// squareOfTheNumber(5);
 
-// // c 
-// for (f = 7; f <= 22; f++) {
-//     console.log(f);
+// // // 3.2
+
+// function returnSquareOfTheNumber(c) {
+//   return c * c;
 // }
 
-// //  Задание 4 
-// const obj = {
-//     'Коля': '200',
-//     'Вася': '300',
-//     'Петя': '400',
-// }
+// console.log(returnSquareOfTheNumber(7));
 
-// for (const hillbilly in obj) {
-//     if (hillbilly === 'Вася') {
-//         break
+// // // Зажание 4 
+
+// function greeting() {
+//   let age = parseInt(prompt("Сколько вам лет?"));
+//   // let age = Number(prompt("Сколько вам лет?"));
+//   if (isNaN(age)) {
+//     console.log("Вы ввели неправильное значение");
+// } else {
+//     if (age < 0) {
+//         console.log("Вы ввели неправильное значение");
+//     } else if (age <= 12) {
+//         console.log("Привет, друг!");
+//     } else {
+//         console.log("Добро пожаловать!");
 //     }
-//     console.log(`${hillbilly} — зарплата ${obj[hillbilly]} долларов`);  
+// }
 // }
 
-// //  Задание 5 
-// // a
-// let n = 1000;
-// let num = 0;
+// greeting();
 
-// while (n >= 50 ) {
-//     n = n / 2;
-//     num++
-//     console.log(num);
+// // // Задание 5 
+
+// function task5(a, b) {
+//   if (isNaN(a) || isNaN(b)) {
+//     return 'Одно или оба значения не являются числом';
+//   } else {
+//     return a * b;
+//   }
 // }
 
-// // b
-// let n1 = 1000;
-// let num1 = 0;
+// console.log(task5(3, 'fhfdh'));
 
-// do {
-//     console.log(num1);
-//     n1 /= 2;
-//     num1 += 1;
-// } while (n1 >= 50);
+// // //  Задание 6 
 
-// // c
-// let num2 = 0;
-// for (n2 = 1000; n2 >= 50; num2 = num2 + 1 ) {
-//     n2 /= 2;
-//     console.log(num2);
+// function toCube() {
+
+//   let a = prompt('Введите число');
+
+//   if (isNaN(a)) {
+//     return 'Переданный параметр не является числом';
+//   } else {
+//     let cube = Number(a) ** 3;
+//     // let cube = Math.pow(Number(a), 3);
+//     return a + ' в кубе равняется ' + cube; 
+//   }
 // }
 
-// Задание 6 
-// a 
-let firstFriday = 1;
-let day = firstFriday;
+// for (a = 0; a <= 10; a++) {
+//   console.log(toCube());
+// }
 
-while (day <= 31) {
-    if ( day % 7 === 0) {
-        console.log(`Сегодня пятница, ${day}е число. Необходимо подготовить отчет.`);
+// // Задание 7 
+
+// function getCircleArea() {
+//   return 3.14 * this.radius ** 2;
+// }
+
+// function getCirclePerimeter () {
+//   return 2 * 3.14 * this.radius;
+// }
+
+// const circle1 = {
+//   radius: 4,
+//   getArea: getCircleArea,
+//   getPerimeter: getCirclePerimeter,
+// }
+
+// const circle2 = {
+//   radius: 5,
+//   getArea: getCircleArea,
+//   getPerimeter: getCirclePerimeter,
+// }
+
+// console.log(circle1.getArea());
+// console.log(circle1.getPerimeter());
+
+// console.log(circle2.getArea());
+// console.log(circle2.getPerimeter());
+
+// Game 1 
+
+function game1() {
+
+    let numberMonth = parseIntprompt('Введите номер месяца (от 1 до 12):');
+
+    // let numberMonth = Numbert('Введите номер месяца (от 1 до 12):');
+
+    if (!isNaN(numberMonth) && numberMonth >= 1 && numberMonth <= 12) {
+        if (numberMonth >= 3 && numberMonthnumberMonth <= 5) {
+            return "Весна";
+        } else if (numberMonth>= 6 && numberMonth <= 8) {
+            return "Лето";
+        } else if (numberMonth >= 9 && numberMonth <= 11) {
+            return "Осень";
+        } else {
+            return "Зима";
+        }
+    } else {
+        return "Некорректный ввод. Введите номер месяца от 1 до 12.";
     } 
-    day += 7;
 }
-
-// b
-let firstFriday1 = 7;
-let day1 = firstFriday1;
-
-do {
-    if ( day1 % 7 === 0) {
-        console.log(`Сегодня пятница, ${day1}е число. Необходимо подготовить отчет.`);
-    }
-    day1 += 7;
-} while (day1 <= 31); 
-
-// c 
-let firstFriday2 = 3;
-
-for (let day2 = firstFriday2; day2 <= 31; day2 += 7) {
-    console.log(`Сегодня пятница, ${day2}е число. Необходимо подготовить отчет.`);
-}
-
-
-
